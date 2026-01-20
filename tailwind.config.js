5081bfdd918768006d999e8a2aa7b58a96e9b3e7
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2026-01-20 12:40:25 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}", // Ye line sabse zaroori hai
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // Ye line sabse important hai
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#1ed760", // Spotify Green
+        dark: "#121212",
+        darker: "#000000",
+        graytext: "#b3b3b3"
+      },
       animation: {
-        'spin-slow': 'spin 8s linear infinite',
+        shimmer: "shimmer 1.5s infinite linear",
+        spin: "spin 1s linear infinite",
+      },
+      keyframes: {
+        shimmer: {
+          "0%": { backgroundPosition: "-200% 0" },
+          "100%": { backgroundPosition: "200% 0" },
+        },
       },
     },
   },
