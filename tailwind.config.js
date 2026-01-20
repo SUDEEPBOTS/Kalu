1eb34dd70443a62f6427f2bdff808c284f7408e2
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2026-01-20 12:29:23 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,14 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // Ye line sabse zaroori hai
   ],
   theme: {
     extend: {
       animation: {
         'spin-slow': 'spin 8s linear infinite',
-      }
+      },
     },
   },
   plugins: [],
 };
-
